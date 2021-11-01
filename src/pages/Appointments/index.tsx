3b5031d908a2f623597158b99cc9d2404e5ee42b
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-11-01 18:43:23 -0300

    feat: incremente dinamic data in calendar

--- src/pages/Appointments/index.tsx
+++ src/pages/Appointments/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
@@ -7,8 +8,99 @@ import { FloatingMenu } from '../../components/FloatingMenu'
 import { SimpleButton } from '../../components/SimpleButton'
 import { AppointmentCard } from '../../components/AppointmentCard'
 
+import { data } from '../../utils/dayAndMonths'
+
+interface AppointmentOfDayProps {
+  day: number,
+  dayFormatted: string
+  month: number,
+  monthFormatted: string
+  year: number
+}
 
 export function Appointments() {
+  const today = new Date()
+
+  const [currentYear, setCurrentYear] = useState(today.getFullYear())
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth())
+  const [appointmentOfDay, setAppointmentOfDay] = useState({} as AppointmentOfDayProps)
+
+  const currentDay = today.getDate()
+
+  let currentMonthFormatted = ''
+  let todayMonthFormatted = ''
+  let currentDayFormatted = ''
+  let daysOfMonth = []
+
+  const { weekDays, months } = data
+
+  months.find(month => month.key === currentMonth && (currentMonthFormatted = month.title))
+  months.find(month => month.key === today.getMonth() && (todayMonthFormatted = month.title))
+
+  const currentDayString = String(currentDay)
+  currentDayString.length === 1 ? currentDayFormatted = `0${currentDayString}` : currentDayFormatted = currentDayString
+
+
+  const dayOfMonth = new Date(currentYear, currentMonth, 1)
+  let contDate = 0
+  let dayKey = 0
+
+  while (dayOfMonth.getMonth() === currentMonth) {
+    const dayInWeek = dayOfMonth.getDay()
+    dayKey++
+
+    if (dayOfMonth.getDate() === 1 && dayInWeek > 0) {
+
+      while (contDate < dayInWeek) {
+        contDate++
+        daysOfMonth.push({
+          dayKey: dayKey++,
+          day: null,
+          dayOfWeek: dayInWeek
+        })
+      }
+    }
+
+    daysOfMonth.push({
+      dayKey,
+      fullDate: new Date(currentYear, currentMonth, contDate++),
+      day: dayOfMonth.getDate(),
+      dayOfWeek: dayInWeek
+    })
+
+    dayOfMonth.setDate(dayOfMonth.getDate() + 1)
+
+  }
+
+  function handleAlterMonth(type: 'increment' | 'decrement') {
+    type === 'increment' && setCurrentMonth(currentMonth + 1)
+    type === 'decrement' && setCurrentMonth(currentMonth - 1)
+
+    if (type === 'increment' && currentMonth === 11) {
+      setCurrentYear(currentYear + 1)
+      setCurrentMonth(0)
+    }
+
+    if (type === 'decrement' && currentMonth === 0) {
+      setCurrentYear(currentYear - 1)
+      setCurrentMonth(11)
+    }
+  }
+
+  function handleAlterAppointmentsDay(day: number) {
+    const dayString = String(day)
+
+    const data = {
+      day,
+      dayFormatted: dayString.length === 1 ? `0${day}` : String(day),
+      month: currentMonth,
+      monthFormatted: currentMonthFormatted,
+      year: currentYear
+    }
+
+    setAppointmentOfDay(data)
+  }
+
   return (
     <main className={styles.container}>
       <section className={styles.calendar_container}>
@@ -26,11 +118,14 @@ export function Appointments() {
         <main className={styles.content}>
           <header className={styles.selectMonthContainer}>
             <div>
-              <button className={styles.previousMonth}>
-                <FiChevronLeft />
-              </button>
-              <span className={styles.monthTitle}>Outubro de 2021</span>
-              <button className={styles.nextMonth}>
+              {currentMonth === today.getMonth() && currentYear === today.getFullYear()
+                ? ''
+                : <button className={styles.previousMonth} onClick={() => handleAlterMonth('decrement')}>
+                  <FiChevronLeft />
+                </button>
+              }
+              <span className={styles.monthTitle}>{currentMonthFormatted} de {currentYear}</span>
+              <button className={styles.nextMonth} onClick={() => handleAlterMonth('increment')}>
                 <FiChevronRight />
               </button>
             </div>
@@ -39,244 +134,56 @@ export function Appointments() {
           <section className={styles.monthContainer}>
             <div className={styles.monthContent}>
               <header className={styles.weekDayContainer}>
-                <strong className={`${styles.dayTitle} ${styles.weekend}`}>Dom</strong>
-                <strong className={styles.dayTitle}>Seg</strong>
-                <strong className={styles.dayTitle}>Ter</strong>
-                <strong className={styles.dayTitle}>Qua</strong>
-                <strong className={styles.dayTitle}>Qui</strong>
-                <strong className={styles.dayTitle}>Seg</strong>
-                <strong className={`${styles.dayTitle} ${styles.weekend}`}>Sáb</strong>
+                {
+                  weekDays.map(day => (
+                    <strong
+                      key={day.key}
+                      className={day.key === 0 || day.key === 6
+                        ? `${styles.dayTitle} ${styles.weekend}`
+                        : styles.dayTitle
+                      }>
+                      {day.short}
+                    </strong>
+
+                  ))
+                }
               </header>
 
               <main className={styles.dayContainer}>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>0</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>0</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>0</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>0</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>0</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
 
+                {daysOfMonth.map((day) => (
+                  day.day === null
+                    ? <div key={day.dayKey}></div>
+                    : <button
+                      key={day.dayKey}
+                      className={`
+                        ${styles.dayButton} 
+                        ${day.dayOfWeek === 0 && styles.isWeekend} ${day.dayOfWeek === 6 && styles.isWeekend}
+                        ${day.day === appointmentOfDay.day
+                        && appointmentOfDay.month === currentMonth
+                        && currentYear === appointmentOfDay.year
+                        && styles.isActive}
+                        `
+                      }
+                      onClick={() => handleAlterAppointmentsDay(day.day)}
+                    >
+                      <span
+                        className={`${styles.day} 
+                        ${today.getDate() === day.day
+                          && today.getMonth() === day.fullDate.getMonth()
+                          && today.getFullYear() === day.fullDate.getFullYear()
+                          && styles.toDay}`}
+                      >
+                        {day.day}
+                      </span>
+                      <div className={styles.appointmentTagContainer}>
+                        {/* <span className={styles.tag} />
+                        <span className={styles.tag} /> */}
+                      </div>
 
+                    </button>
 
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>1</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>2</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>3</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>4</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>5</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>6</span>
-                  <div className={styles.appointmentTagContainer}>
-                    <span className={styles.tag} />
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>7</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>8</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>9</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>10</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>11</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>12</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>13</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>14</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>15</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>16</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button><button className={styles.dayButton}>
-                  <span className={styles.day}>17</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>18</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>19</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>20</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>21</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>22</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>23</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>24</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>25</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>26</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>27</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>28</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>29</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>30</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-                <button className={styles.dayButton}>
-                  <span className={styles.day}>31</span>
-                  <div className={styles.appointmentTagContainer}>
-
-                  </div>
-                </button>
-
+                ))}
 
               </main>
             </div>
@@ -293,7 +200,12 @@ export function Appointments() {
 
       <section className={styles.appointments_container}>
         <header className={styles.appointmentTitleDay}>
-          <h2 className={styles.appointmentTitle}>29 de Outubro de 2021</h2>
+          <h2 className={styles.appointmentTitle}>
+            {appointmentOfDay.day
+              ? `${appointmentOfDay?.dayFormatted} de ${appointmentOfDay?.monthFormatted} de ${appointmentOfDay?.year}`
+              : `${currentDayFormatted} de ${todayMonthFormatted} de ${today.getFullYear()}`
+            }
+          </h2>
         </header>
 
         <main className={styles.appointmentsContainer}>
@@ -303,7 +215,25 @@ export function Appointments() {
           <div className={styles.nextAppointment}>
             <AppointmentCard type='emphasis' />
           </div>
-          <AppointmentCard />
+
+          <div className={styles.nextAppointmentsContainer}>
+            <header className={styles.nextAppointmentsOptions}>
+              <button className={`${styles.appointmentButton} ${styles.buttonActive}`}>Próximos</button>
+              <button className={styles.appointmentButton}>Abertos</button>
+              <button className={styles.appointmentButton}>Finalizados</button>
+              <button className={styles.appointmentButton}>Cancelados</button>
+            </header>
+
+            <div className={styles.appointmentsListContainer}>
+              <AppointmentCard />
+              <AppointmentCard />
+              <AppointmentCard />
+              <AppointmentCard />
+              <AppointmentCard />
+
+
+            </div>
+          </div>
         </main>
       </section>
     </main>
---
+++ src/utils/dayAndMonths.ts
@@ -0,0 +1,90 @@
+export const data = {
+  months: [
+    {
+      key: 0,
+      title: 'Janeiro'
+    },
+    {
+      key: 1,
+      title: 'Fevereiro'
+    },
+    {
+      key: 2,
+      title: 'Março'
+    },
+    {
+      key: 3,
+      title: 'Abril'
+    },
+    {
+      key: 4,
+      title: 'Maio'
+    },
+    {
+      key: 5,
+      title: 'Junho'
+    },
+    {
+      key: 6,
+      title: 'Julho'
+    },
+    {
+      key: 7,
+      title: 'Agosto'
+    },
+    {
+      key: 8,
+      title: 'Setembro'
+    },
+    {
+      key: 9,
+      title: 'Outubro'
+    },
+    {
+      key: 10,
+      title: 'Novembro'
+    },
+    {
+      key: 11,
+      title: 'Dezembro'
+    }
+  ],
+  weekDays: [
+    {
+      key: 0,
+      title: 'Domingo',
+      short: 'Dom'
+    },
+    {
+      key: 1,
+      title: 'Segunda',
+      short: 'Seg'
+    },
+    {
+      key: 2,
+      title: 'Terça',
+      short: 'Ter'
+    },
+    {
+      key: 3,
+      title: 'Quarta',
+      short: 'Qua'
+    },
+    {
+      key: 4,
+      title: 'Quinta',
+      short: 'Qui'
+    },
+    {
+      key: 5,
+      title: 'Sexta',
+      short: 'Sex'
+    },
+    {
+      key: 6,
+      title: 'Sábado',
+      short: 'Sáb'
+    },
+
+  ]
+}
